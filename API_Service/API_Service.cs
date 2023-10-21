3495c16fbfe1345d50ac23c91faa7617917a1b15
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2023-10-20 20:49:57 -0500

    IAPI_Service.cs [Añadido] y API_Service.cs [Modificiado - Primera configuración]

--- API_Service/API_Service.cs
+++ API_Service/API_Service.cs
@@ -1,10 +1,81 @@
-﻿namespace MVC_Productos.API_Service
+﻿using Ejemplo1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace MVC_Productos.API_Service
 {
-    public class API_Service
+    public class API_Service<T> : Controller, IAPI_Service<T>
     {
-        private string? _id;
-        public string ID { get { return _id; } set {  _id = value; } }
-    
+        //Atributos
+        static HttpClient client = new HttpClient();
+        private string _url = "http://localhost:5129/api/Producto";
+        
+        //Propiedades
+        public string URL { get { return _url; } set {  _url = value; } }
+        
+        //Constructor
         public API_Service() { }
+
+        //Métodos
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:5129/api/Producto");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task<IActionResult> Get()
+        {
+            List<T>? lista = null;
+            HttpResponseMessage response = await client.GetAsync(_url);
+            if (response.IsSuccessStatusCode)
+            {
+                lista = await response.Content.ReadAsAsync<List<T>>();
+            }
+            return View(lista);
+        }
+
+        public async Task<IActionResult> Get(int id)
+        {
+            T? entity;
+            HttpResponseMessage response = await client.GetAsync(_url + id);
+            if (response.IsSuccessStatusCode)
+            {
+                entity = await response.Content.ReadAsAsync<T>();
+                return View(entity);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Post(T entity)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(_url, entity);
+            response.EnsureSuccessStatusCode();
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Post(int id)
+        {
+            T? entity;
+            HttpResponseMessage response = await client.GetAsync(_url+ id);
+            if (response.IsSuccessStatusCode)
+            {
+                entity = await response.Content.ReadAsAsync<T>();
+                return View(entity);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(_url + id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ API_Service/IAPI_Service.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Productos.API_Service
+{
+    public interface IAPI_Service<T>
+    {
+        //método GET                (index)
+        public Task<IActionResult> Get();
+
+        //método GET {id}           (details)
+        public Task<IActionResult> Get(int id); 
+
+        //método POST {T}           (create)
+        public Task<IActionResult> Post(T entity);
+
+        //método POST {id}          (edit)
+        public Task<IActionResult> Post(int id);
+
+        //método DELETE {id}        (delete)
+        public Task<IActionResult> Delete(int id);
+
+    }
+}
